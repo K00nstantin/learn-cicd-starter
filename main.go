009fe2114a38ee0779062c5794b212e921d0a28d
Konstantin <kostyaprozorovofficial@yandex.ru>
2026-09-22 20:08:57 +0300

    fixed_security

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"embed"
 	"io"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -89,10 +91,12 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: 5 * time.Second,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger.Info("starting server", "port", port)
 	log.Fatal(srv.ListenAndServe())
 }
